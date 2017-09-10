98d7597a89550d258fdb9acba37bc1ebdf041fe9
nvo87 <nvo87@yandex.ru>
2017-09-10 19:44:41 +0300

    checking command line arguments are added.

--- pprint_json.py
+++ pprint_json.py
@@ -4,8 +4,7 @@ import shutil
 import sys
 
 
-def load_data(filepath):
-    # из-за проблем с кодировкой в utf-8 используем ф-ию io.open
+def load_data_with_utf8(filepath):
     with io.open(filepath, encoding='utf-8') as f_obj:
         return json.load(f_obj)
 
@@ -20,13 +19,27 @@ def pretty_print_json_to_file(json_raw_string, output_filepath):
                   indent=4, ensure_ascii=False)
 
 
-if __name__ == '__main__':
-    # получаем имя файла из аргумента указанного при вызове в консоли
-    filepath = sys.argv[1]
-    json_without_format = load_data(filepath)
+def get_arguments_from_command_line(arg_number):
+    if len(sys.argv) > arg_number:
+        return sys.argv[arg_number]
 
-    if sys.argv[2] == '-f':
-        pretty_print_json_to_file(json_without_format, 'output.json')
+if __name__ == '__main__':
+    filepath = get_arguments_from_command_line(1)
+    if filepath is None:
+        print("You have to type json source filepath")
+        sys.exit()
+
+    try:
+        json_without_format = load_data_with_utf8(filepath)
+    except FileNotFoundError:
+        print("This filepath doesn't exist!")
+        sys.exit()
 
     json_in_pretty_format = get_json_in_pretty_format(json_without_format)
     print(json_in_pretty_format)
+
+    write_to_file_flag = get_arguments_from_command_line(2)
+    if write_to_file_flag == '-f':
+        pretty_print_json_to_file(json_without_format, 'output.json')
+    else:
+        print("Unknown flag. If you want to output result to file, use -f key.")
